cbfb3f3491347a3cc6dc4c12fcad071c4bdcd11d
Maveluke <130248289+Maveluke@users.noreply.github.com>
2023-09-11 20:39:46 -0400

    Main.java created

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,9 +1,9 @@
 public class Multiples {
     public static void main(String[] args) {
         int n = 1000;
-        int m3 = 1000/3;
-        int m5 = 1000/5;
-        int m15 = 1000/15;
+        int m3 = n/3;
+        int m5 = n/5;
+        int m15 = n/15;
         System.out.println(m3 + m5 - m15);
     }
 }
