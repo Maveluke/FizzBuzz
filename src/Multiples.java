edd7a61c1917a953673736080e649914376383ab
Maveluke <maveluk.cy@gmail.com>
2023-09-15 00:57:04 -0400

    Fix Multiples.java and Reduce.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,9 +1,17 @@
 public class Multiples {
+    private static int gcd(int a, int b) {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+    private static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
     public static void main(String[] args) {
-        int n = 1000;
-        int m3 = n/3;
-        int m5 = n/5;
-        int m15 = n/15;
-        System.out.println(m3 + m5 - m15);
+        int n = 10, m1 = 3, m2 = 5;
+        int d1 = (n - 1) / m1;
+        int d2 = (n - 1) / m2;
+        int d1x2 = (n - 1) / lcm(m1, m2);
+        System.out.println(d1 + d2 - d1x2);
     }
 }
